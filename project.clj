22e65f14530ea1e48b60635ecd97e56f2dfb169d
David Ellison <davidehellison@gmail.com>
2015-04-04 18:19:10 -0700

    adding project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rind "0.1.0-SNAPSHOT"
   :description "Clojure code for naive bayes text classification (i.e. Spam detection)."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/dellison/Rind"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
