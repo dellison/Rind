c50309310d5ecd91e284c72d6dd59c464d670faa
David Ellison <davidehellison@gmail.com>
2015-04-04 17:20:12 -0700

    adding MIT license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject rind "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure code for naive bayes text classification (i.e. Spam detection)."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :jvm-opts ["-Xmx2g"]
   :main rind.core)
